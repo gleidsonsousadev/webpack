7d97a2dbb826ec4bae80e2be0dd802d485995962
Gleidson Sousa <gleidsonsousadev@gmail.com>
2022-08-29 21:56:43 -0300

    meu primeiro commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { dobro, bomDia, aluno } from "./nayra.js";
+import "./estilo.scss";
+import "bootstrap";
+
+const teste = (i) => i * 2;
+
+console.log(teste(5));
+console.log(bomDia(aluno));
+console.log(`o dobro de 10 é ${dobro(10)}`);
